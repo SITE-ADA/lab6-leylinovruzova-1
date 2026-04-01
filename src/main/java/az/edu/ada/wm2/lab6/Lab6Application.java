ed6883e1ad2c3fdb79549c5dfba5dbdf7fb110c5
leylinovruzova <lnovruzova18683@ada.edu.az>
2026-04-02 03:55:54 +0400

    Add Services

--- src/main/java/az/edu/ada/wm2/lab6/Lab6Application.java
+++ src/main/java/az/edu/ada/wm2/lab6/Lab6Application.java
@@ -1,13 +1,11 @@
-package az.edu.ada.wm2.lab6;
+package az.edu.ada.wm2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Lab6Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Lab6Application.class, args);
 	}
-
 }
--- src/test/java/az/edu/ada/wm2/service/CategoryServiceImplTest.java
+++ src/test/java/az/edu/ada/wm2/service/CategoryServiceImplTest.java
@@ -1,5 +1,6 @@
 package az.edu.ada.wm2.service;
 
+import az.edu.ada.wm2.lab6.model.Category;
 import az.edu.ada.wm2.lab6.model.Product;
 import az.edu.ada.wm2.lab6.model.dto.CategoryRequestDto;
 import az.edu.ada.wm2.lab6.model.dto.CategoryResponseDto;
@@ -8,6 +9,7 @@ import az.edu.ada.wm2.lab6.model.mapper.ProductMapper;
 import az.edu.ada.wm2.lab6.repository.CategoryRepository;
 import az.edu.ada.wm2.lab6.repository.ProductRepository;
 
+import az.edu.ada.wm2.lab6.service.CategoryServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
