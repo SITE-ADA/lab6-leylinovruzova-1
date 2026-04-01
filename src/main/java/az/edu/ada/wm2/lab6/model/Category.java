7b0da7a3f7d590093601f490827842ea53492621
leylinovruzova <lnovruzova18683@ada.edu.az>
2026-04-02 03:09:31 +0400

    Add Mappers

--- src/main/java/az/edu/ada/wm2/lab6/model/Category.java
+++ src/main/java/az/edu/ada/wm2/lab6/model/Category.java
@@ -6,9 +6,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,12 +18,16 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Category {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
 
     @ManyToMany(mappedBy = "categories")
-    private List<Product> products;
+    @Builder.Default
+    private List<Product> products = new ArrayList<>();
 }
